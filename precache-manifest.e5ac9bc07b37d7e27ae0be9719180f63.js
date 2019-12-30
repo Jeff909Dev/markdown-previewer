9c8630dc7f87f6c35b74baa7005d34a9770da5ab
Jeffrey Viveros <temel57@grupotemel.net>
2019-12-30 11:28:22 +0100

    Updates

---
+++ precache-manifest.e5ac9bc07b37d7e27ae0be9719180f63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee66659c993c11e5d1ceee2efa70ecf0",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "5fc3764098a3ccad7e1f",
+    "url": "/markdown-previewer/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "9ebe3f0c7464e349c8a7",
+    "url": "/markdown-previewer/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5fc3764098a3ccad7e1f",
+    "url": "/markdown-previewer/static/js/2.03c2f2f4.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/markdown-previewer/static/js/2.03c2f2f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ebe3f0c7464e349c8a7",
+    "url": "/markdown-previewer/static/js/main.bfba6785.chunk.js"
+  },
+  {
+    "revision": "7de858448200f52ccd17",
+    "url": "/markdown-previewer/static/js/runtime-main.839dde88.js"
+  }
+]);
